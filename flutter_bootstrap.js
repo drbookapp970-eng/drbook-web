b426aa79965dc05be3b1c3996231d0af2ff61b6c
drbookapp970-eng <drbookapp970@gmail.com>
2026-07-29 21:41:02 +0530

    Fix Flutter web build and update generated web assets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1545190187" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1566285040" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
